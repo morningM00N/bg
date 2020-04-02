1daafca518b9f1cc6927d5bc9fbdc40d76906ddd
morningm00n <abcdeei888@gmail.com>
2020-04-03 02:28:08 +0900

    commit

--- secrethitlerboard.js
+++ secrethitlerboard.js
@@ -1,4 +1,21 @@
+let userfullscreen = confirm("꽉찬 화면으로 표시하겠습니까?\n(화면이 깨질 수 있습니다.)")
+//let userfullscreen = false
+
+if (userfullscreen==true)
+{
+    funcWidthPerHeight(0)
+}
+else{
+
 funcWidthPerHeight(4096 / 2918)
+}
+
+var doResize = true
+$(window).resize(function () {
+    funcUpdatePageSize(true)
+    fucDrawSecretHitlerBoard()
+});
+
 
 funcUpdatePageSize(true)
 
@@ -11,7 +28,7 @@ function funcChangeFascistBrd() {
     if (fascisttrackIdx == 4) {
         fascisttrackIdx = 1
     }
-    funcInsertElement("imgFascistBoard", "img", "null", 0, 0.5, 1, 1, 4096 / 2918).src = "img/secrethitler/fascisttrack" + fascisttrackIdx + ".png"
+    document.getElementById("imgFascistBoard").src = "img/secrethitler/fascisttrack" + fascisttrackIdx + ".png"
 }
 
 let arrLiberalEnacted = new Array()
@@ -21,16 +38,19 @@ let arrFascistEnacted = new Array()
 let curElectionTracker = 0
 
 function fucDrawSecretHitlerBoard() {
-    // let imgLiberalBoard = funcInsertElement("imgLiberalBoard", "img", "null", 0, 0, 1, 1, 4096 / 2918)
-    // imgLiberalBoard.src = "img/secrethitler/liberaltrack.png"
-    // funcInsertElement("imgFascistBoard", "img", "null", 0, 0.5, 1, 1, 4096 / 2918).src = "img/secrethitler/fascisttrack" + fascisttrackIdx + ".png"
+    
+
+     let imgLiberalBoard = funcInsertElement("imgLiberalBoard", "img", "null", 0, 0, 1, 1, pageWidth/pageHeight*2)
+     imgLiberalBoard.src = "img/secrethitler/liberaltrack.png"
+     funcInsertElement("imgFascistBoard", "img", "null", 0, 0.5, 1, 1, pageWidth/pageHeight*2).src = "img/secrethitler/fascisttrack" + fascisttrackIdx + ".png"
 
     let btnChangeFascistBrd = funcInsertElement("btnChangeFascistBoard", "button", "btnTrans",
-        0.9, 0.3, 1.0, 0.4)
-    btnChangeFascistBrd.innerHTML = "chage Board"
+    0.4037, 0.5474, 0.5967, 0.6170
+        )
+    //btnChangeFascistBrd.innerHTML = "chage Board"
     btnChangeFascistBrd.onclick = funcChangeFascistBrd
 
-    let locArr = new Array(0.1488, 0.1136, 0.2924, 0.3807)
+    let locArr = new Array(0.1540, 0.1136, 0.2924, 0.3807)
 
     let length = locArr[2] - locArr[0];
     for (let idx = 0; idx < 5; idx++) {
@@ -46,20 +66,20 @@ function fucDrawSecretHitlerBoard() {
                 event.srcElement.style.backgroundImage = "url('')"
             } else {
                 arrLiberalEnacted[idx] = true
-                event.srcElement.style.backgroundImage = "url('img/secrethitler/policycards.png')"
+                event.srcElement.style.backgroundImage = "url('img/secrethitler/liberalpolicy.png')"
                 let btnElectTrack = document.getElementById("btnElect" + curElectionTracker)
                 btnElectTrack.style.backgroundColor = "transparent"
                 btnElectTrack.style.boxShadow = " 0px 0px transparent"
 
                 curElectionTracker = 0
                 btnElectTrack = document.getElementById("btnElect" + curElectionTracker)
-                btnElectTrack.style.backgroundColor = "black"
+                btnElectTrack.style.backgroundColor = "#2980B9"
                 btnElectTrack.style.boxShadow = shadowOffset + "px " + shadowOffset + "px gray"
             }
         }
     }
 
-    locArr = new Array(0.1488, 0.6136, 0.2724, 0.8807)
+    locArr = new Array(0.0886, 0.6228, 0.22652, 0.8878)
     length = locArr[2] - locArr[0];
     for (let idx = 0; idx < 6; idx++) {
         //let btnLiberal = 
@@ -74,7 +94,7 @@ function fucDrawSecretHitlerBoard() {
                 event.srcElement.style.backgroundImage = "url('')"
             } else {
                 arrFascistEnacted[idx] = true
-                event.srcElement.style.backgroundImage = "url('img/secrethitler/policycards.png')"
+                event.srcElement.style.backgroundImage = "url('img/secrethitler/fascistpolicy.png')"
 
                 let btnElectTrack = document.getElementById("btnElect" + curElectionTracker)
                 btnElectTrack.style.backgroundColor = "transparent"
@@ -82,7 +102,7 @@ function fucDrawSecretHitlerBoard() {
 
                 curElectionTracker = 0
                 btnElectTrack = document.getElementById("btnElect" + curElectionTracker)
-                btnElectTrack.style.backgroundColor = "black"
+                btnElectTrack.style.backgroundColor = "#2980B9"
                 btnElectTrack.style.boxShadow = shadowOffset + "px " + shadowOffset + "px gray"
 
 
@@ -90,13 +110,13 @@ function fucDrawSecretHitlerBoard() {
         }
     }
 
-    locArr = new Array(0.2, 0.5, 0.3, 0.6)
-    length = 0.2
+    locArr = new Array(0.3321, 0.3907, 0.3707, 0.4429)
+    length = 0.094
 
-    let shadowOffset = 0.03 * pageWidth
+    let shadowOffset = 0.004 * pageWidth
     for (let idx = 0; idx < 4; idx++) {
         //let btnLiberal = 
-        funcInsertElement("btnElect" + idx, "button", "btnTrans",
+        funcInsertElement("btnElect" + idx, "button", "btnCircle",
             locArr[0] + idx * length,
             locArr[1],
             locArr[2] + idx * length,
@@ -109,14 +129,16 @@ function fucDrawSecretHitlerBoard() {
 
                 curElectionTracker = idx
                 btnElectTrack = document.getElementById("btnElect" + curElectionTracker)
-                btnElectTrack.style.backgroundColor = "black"
+                btnElectTrack.style.backgroundColor = "#2980B9"
                 btnElectTrack.style.boxShadow = shadowOffset + "px " + shadowOffset + "px gray"
             }
         }
     }
     let btnElectTrack = document.getElementById("btnElect" + curElectionTracker)
-    btnElectTrack.style.backgroundColor = "black"
+    btnElectTrack.style.backgroundColor = "#2980B9"
     btnElectTrack.style.boxShadow = shadowOffset + "px " + shadowOffset + "px gray"
+    funcInsertFullScreenButton(0.02,0.47,0.08,0.525)
+    
 
 
 }
