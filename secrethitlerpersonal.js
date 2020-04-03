170299b40fc32187341fa97618fb9db9a7bf2579
eettrrr <52900663+eettrrr@users.noreply.github.com>
2020-04-03 21:15:28 +0900

    Update secrethitlerpersonal.js

--- secrethitlerpersonal.js
+++ secrethitlerpersonal.js
@@ -1,20 +1,12 @@
-let temppageHeight = document.documentElement.clientHeight
-let temppageWidth = document.documentElement.clientWidth
-let ratio = temppageWidth / temppageHeight
-if (ratio < 1) {
-    ratio = 1 / ratio
-}
+//funcPrepareGetLocation()
 
-funcWidthPerHeight(ratio)
+funcWidthPerHeight(0)
 
-funcUpdatePageSize(true, true)
+funcUpdatePageSize(true)
 
 $(window).resize(function() {
-    if (doResize == false) {
-        return
-    }
-    funcUpdatePageSize(true, true)
-    funcRelocateElements()
+    funcUpdatePageSize(true)
+    funcDrawSHPersonal()
 });
 
 
@@ -40,23 +32,58 @@ for (let idx = playerNameList.length - 1; idx >= 0; idx--) {
 
 console.log(playerNameList)
 
+let roomNumber = "방번호"
+let numOfPlayers = "수"
+let idxOfPlayer = 0
+
 function funcDrawSHPersonal() {
-    let inputRoom = funcInsertElement("inputRoom", "input", "sltTrans",
-        0, 0, 0.4, 0.1)
-    inputRoom.value = "방번호"
-    let btnNumOfP = funcInsertElement("btnNumOfP", "p", "sltTrans",
-        0.5, 0, 0.7, 0.111)
-    btnNumOfP.innerHTML = 5
-    btnNumOfP.onclick = function() {
-        let numOfPlayer = Number(event.srcElement.innerHTML) + 1
-        if (numOfPlayer == 11) {
-            numOfPlayer = 5
+    if (pageWidth > pageHeight) {
+        for (let idx = 0; idx < nameOfRelocatedElements.length; idx++) {
+            document.getElementById(nameOfRelocatedElements[idx]).style.display = "none"
         }
-        event.srcElement.innerHTML = numOfPlayer
+        let pWarning = funcInsertElement("pWarning", "p", "sltTrans", 0.1, 0.1, 0.9, 0.2)
+        pWarning.innerHTML = "세로 모드로 실행해 주세요"
+        pWarning.style.display = "inline"
+        return
+    }
+    for (let idx = 0; idx < nameOfRelocatedElements.length; idx++) {
+        document.getElementById(nameOfRelocatedElements[idx]).style.display = "inline"
+    }
+    let pWarning = document.getElementById("pWarning")
+    if (pWarning != null) {
+        pWarning.style.display = "none"
+    }
+
+
+    let imgBG = funcInsertElement("imgBG", "img", null, 0, 0, 1, 1)
+        //imgBG.src = "img/secrethitler/rolebg.png"
+    imgBG.style.opacity = 0.5
+    let inputRoom = funcInsertElement("inputRoom", "input", "sltTrans",
+        0.093, 0.0560, 0.65, 0.16
+    )
+    inputRoom.onchange = function() {
+        roomNumber = event.srcElement.value
+    }
+    inputRoom.value = roomNumber
+    let btnNumOfP = funcInsertElement("btnNumOfP", "p", "sltTrans",
+        0.67, 0.0560, 0.91, 0.16
+    )
+    btnNumOfP.innerHTML = numOfPlayers
+    btnNumOfP.onclick = function() {
+        if (numOfPlayers == "수") {
+            numOfPlayers = 5
+        } else {
+            numOfPlayers++
+            if (numOfPlayers == 11) {
+                numOfPlayers = 5
+            }
+        }
+        event.srcElement.innerHTML = numOfPlayers
 
     }
     let sltNumOfPlayers = funcInsertElement("sltPlayers", "select", "sltTrans",
-        0.8, 0, 1.0, 0.111)
+        0.093, 0.18, 0.65, 0.18 + 0.16 - 0.0560
+    )
 
     let optHead = document.createElement("option")
     optHead.innerHTML = "이름"
@@ -68,6 +95,83 @@ function funcDrawSHPersonal() {
 
         sltNumOfPlayers.appendChild(opt)
     }
+    sltNumOfPlayers.selectedIndex = idxOfPlayer
+    sltNumOfPlayers.onchange = function() {
+        idxOfPlayer = event.srcElement.selectedIndex
+    }
+
+    let btnStart = funcInsertElement("btnStart", "button", "sltTrans",
+        0.67, 0.18, 0.91, 0.18 + 0.16 - 0.0560
+    )
+    btnStart.onclick = funcDrawPolicy
 }
 
+let playerRole
+let policyDeck = new Array(17)
+for (let idx = 0; idx < policyDeck.length; idx++) {
+    policyDeck[idx] = 0
+}
+let numOfDrawDeck = 17
+
+function funcStartGame() {
+    numOfPlayers = 7
+    playerRole = new Array(numOfPlayers)
+
+    let seed = roomNumber + "_" + idxOfPlayer + "_"
+    let d = new Date()
+    seed += (d.getFullYear() + "_")
+    if (d.getHours() >= 1 && d.getHours() < 24) {
+        seed += (d.getMonth() + "_" + d.getDate() + "_")
+    }
+    if (d.getMinutes() >= 5 && d.getMinutes() < 56) {
+        seed += (d.getHours() + "_")
+    }
+
+    console.log(seed)
+    MMath.seedrandom(seed)
+    console.log(MMath.getRandom(7))
+    for (let idx = 0; idx < numOfPlayers; idx++) {
+        let loc = MMath.getRandom(numOfPlayers - 1)
+        while (playerRole[loc] >= 0) {
+            loc = MMath.getRandom(numOfPlayers - 1)
+        }
+        playerRole[loc] = idx
+    }
+    console.log(playerRole)
+
+}
+
+let ENACTED = 2
+let DISCARDED = 1
+let UNUSED = 0
+
+function funcDrawPolicy() {
+    if (numOfDrawDeck < 3) {
+        console.log("reshuffle...")
+        numOfDrawDeck = 0
+        for (let idx = 0; idx < policyDeck.length; idx++) {
+            if (policyDeck[idx] != ENACTED) {
+                policyDeck[idx] = UNUSED
+                numOfDrawDeck++
+            }
+        }
+    }
+    numOfDrawDeck -= 3
+    console.log("remain " + numOfDrawDeck + " cards")
+    for (let idx = 0; idx < 3; idx++) {
+        let thisCard = MMath.getRandom(0, policyDeck.length - 1)
+        while (policyDeck[thisCard] != UNUSED) {
+            thisCard = MMath.getRandom(0, policyDeck.length - 1)
+        }
+        if (thisCard < 6) {
+            console.log(thisCard, "liberal")
+        } else {
+            console.log(thisCard, "fascist")
+        }
+        policyDeck[thisCard] = DISCARDED
+
+    }
+}
 funcDrawSHPersonal()
+
+funcStartGame()
