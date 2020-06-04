34c426b3edd9cf5111828cd8fc0e56882e759c81
eettrrr <52900663+eettrrr@users.noreply.github.com>
2020-06-04 15:06:27 +0900

    Update secrethitlerpersonal.js

--- secrethitlerpersonal.js
+++ secrethitlerpersonal.js
@@ -76,8 +76,9 @@ function funcDrawSHPersonal() {
     let inputRoom = funcInsertElement("inputRoom", "input", "sltTrans",
         0.093, 0.0560, 0.65, 0.16
     )
-    inputRoom.onchange = function() {
-        roomNumber = event.srcElement.value
+    inputRoom.onclick = function() {
+        inputRoom.value = prompt("방번호를 입력하세요.")
+        roomNumber = inputRoom.value
     }
     inputRoom.value = roomNumber
     let btnNumOfP = funcInsertElement("btnNumOfP", "p", "sltTrans",
