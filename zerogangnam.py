5b07300dd1034260a16364048ef54e0857421b81
eettrrr <52900663+eettrrr@users.noreply.github.com>
2021-06-28 22:05:13 +0900

    Update zerogangnam.py

--- zerogangnam.py
+++ zerogangnam.py
@@ -9,31 +9,33 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime, timedelta, date
 
+if len(sys.argv)!=5:
+    print("2021-07-18 이성훈 010-1234-5678 10:46:00")
+    sys.exit()
 
 driver = webdriver.Chrome()
 wait = WebDriverWait(driver, 10)
 
 
-target = "2021-07-18"
-reservName = '김진현'
-reservPhone='010-1234-5678'
-reservTime = '16:25:00'
+
+target = sys.argv[1]
+reservName = sys.argv[2]
+reservPhone=sys.argv[3]
+reservTime = sys.argv[4]
 
 targetdate = date.fromisoformat(target)
 today = date.today()
 referdate = today + timedelta(14)
-
 while referdate.weekday() == 6 or referdate.weekday()==5:
     referdate = referdate- timedelta(1)
 
-print(referdate.__str__())
 
 driver.get("https://zerogangnam.com/reservation")
 wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'body')))
 
 if today.month != referdate.month:
     driver.find_elements_by_class_name("datepicker--nav-action")[1].click()
-time.sleep(1)
+time.sleep(0.5)
 
 reservDays = driver.find_elements_by_class_name("datepicker--cell")
 
@@ -45,14 +47,14 @@ while True:
         break
     idx += 1
 
-time.sleep(1)
+time.sleep(0.5)
 temp = driver.find_element_by_class_name("ste1-theme")
 
 themaList = temp.find_elements_by_css_selector('span')
-time.sleep(1)
+time.sleep(0.5)
 
 themaList[7].click()
-time.sleep(1)
+time.sleep(0.5)
 
 timeList = driver.find_element_by_id('themeTimeWrap').find_elements_by_class_name('hover2')
 
@@ -61,42 +63,47 @@ for _time in timeList:
         _time.click()
         break
 
-time.sleep(1)
+time.sleep(0.5)
 
 driver.find_element_by_id('nextBtn').click()
-time.sleep(1)
+time.sleep(0.5)
 
 inputBox = driver.find_elements_by_class_name('el-input')
 inputBox[0].send_keys(reservName)
 inputBox[1].send_keys(reservPhone)
 
 driver.find_element_by_id('step2PeopleWrap').find_elements_by_css_selector('option')[1].click()
-time.sleep(1)
+time.sleep(0.5)
 driver.find_element_by_class_name('fw7').click()
-time.sleep(1)
+time.sleep(0.5)
 
 js = "document.getElementById('dataReservationDate').value = '"+target+"'"
 driver.execute_script(js)
-time.sleep(1)
+time.sleep(0.5)
 
 if targetdate.weekday() == 5 or targetdate.weekday()==6:
     js = "document.getElementsByName('themePK')[7].value=44"
     driver.execute_script(js)
-    time.sleep(1)
+    time.sleep(0.5)
 
 js = "var timeList = document.getElementsByName('reservationTime'); for(var idx=0; idx<timeList.length; idx++){timeList[idx].value='"+reservTime+"';}"
 driver.execute_script(js)
 
-btnClick = driver.find_element_by_id('reservationBtn')
+btnClick= driver.find_element_by_id('reservationBtn')
 while True:
+    print(datetime.today().__str__())
     try:
+        btnClick.click()
+        wait.until(EC.alert_is_present())
         alert = driver.switch_to.alert
         alert.accept()
-        btnClick.click()
     except:
         time.sleep(0.2)
     try:
         btnClick.click()
     except:
         time.sleep(0.2)
+
+
+
 
