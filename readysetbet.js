26c8f62de4e94af2beb626398e32804dfa6769af
morningm00n <abcdeei888@gmail.com>
2023-12-25 02:04:35 +0900

    mid

--- readysetbet.js
+++ readysetbet.js
@@ -1,19 +1,18 @@
-let userfullscreen = confirm("꽉찬 화면으로 표시하겠습니까?\n(화면이 깨질 수 있습니다.)")
-//let userfullscreen = false
+//let userfullscreen = confirm("꽉찬 화면으로 표시하겠습니까?\n(화면이 깨질 수 있습니다.)")
+let userfullscreen = false
 
-if (userfullscreen==true)
-{
+if (userfullscreen == true) {
     funcWidthPerHeight(0)
 }
-else{
+else {
 
-funcWidthPerHeight(4096 / 2918)
+    funcWidthPerHeight(2717 / 4075)
 }
 
 var doResize = true
 $(window).resize(function () {
     funcUpdatePageSize(true)
-    fucDrawSecretHitlerBoard()
+    fucDrawReadySetBetBoard()
 });
 
 
@@ -21,126 +20,261 @@ funcUpdatePageSize(true)
 
 //funcPrepareGetLocation()
 
-var fascisttrackIdx = 1
+let coinSizePerWidth = 1 / 17
+let coinSizeOrigin = 1 / 11.5
+let shadowDepth = pageWidth / 200
+let shadowDepth2 = pageWidth / 300
 
-function funcChangeFascistBrd() {
-    fascisttrackIdx++
-    if (fascisttrackIdx == 4) {
-        fascisttrackIdx = 1
+let occupied = {}
+
+let colors = ["black", "blue", "brown", "dark", "green", "pink", "purple", "silver", "white", "yellow"]
+
+let exoticOrder=[0,1,2,3,4]
+let propOrder=[]
+
+function initial(){
+    for (let i = 0; i <= 27; i++) {
+        propOrder.push(i)
     }
-    document.getElementById("imgFascistBoard").src = "img/secrethitler/fascisttrack" + fascisttrackIdx + ".png"
+    for (let index = 0; index < 10000; index++) {
+        let idx1 = getRandom(5,0)        
+        let idx2 = getRandom(5,0)
+        let temp = exoticOrder[idx1]
+        exoticOrder[idx1]=exoticOrder[idx2]
+        exoticOrder[idx2]=temp
+
+        let idx3 = getRandom(28,0)
+        let idx4 = getRandom(28,0)
+        temp = propOrder[idx3]
+        propOrder[idx3]=propOrder[idx4]
+        propOrder[idx4]=temp
+    }
 }
+initial()
 
-let arrLiberalEnacted = new Array()
-let arrFascistEnacted = new Array()
+function funcProp(i, color, number) {
+    console.log("funcProp", i)
+    if (i >= 1 && i <= 5) {
+        if (occupied["btnPropChip_" + i] != undefined) {
+            return
+        }
 
+        if (color == undefined) {
+            color = colors[getRandom(colors.length, 0)]
 
-let curElectionTracker = 0
-
-function fucDrawSecretHitlerBoard() {
-    
-
-     let imgLiberalBoard = funcInsertElement("imgLiberalBoard", "img", "null", 0, 0, 1, 1, pageWidth/pageHeight*2)
-     imgLiberalBoard.src = "img/secrethitler/liberaltrack.png"
-     funcInsertElement("imgFascistBoard", "img", "null", 0, 0.5, 1, 1, pageWidth/pageHeight*2).src = "img/secrethitler/fascisttrack" + fascisttrackIdx + ".png"
-
-    let btnChangeFascistBrd = funcInsertElement("btnChangeFascistBoard", "button", "btnTrans",
-    0.4037, 0.5474, 0.5967, 0.6170
-        )
-    //btnChangeFascistBrd.innerHTML = "chage Board"
-    btnChangeFascistBrd.onclick = funcChangeFascistBrd
-
-    let locArr = new Array(0.1540, 0.1136, 0.2924, 0.3807)
-
-    let length = locArr[2] - locArr[0];
-    for (let idx = 0; idx < 5; idx++) {
-        //let btnLiberal = 
-        funcInsertElement("btnLiberal" + idx, "button", "btnTrans",
-            locArr[0] + idx * length,
-            locArr[1],
-            locArr[2] + idx * length,
-            locArr[3]
-        ).onclick = function() {
-            if (arrLiberalEnacted[idx] == true) {
-                arrLiberalEnacted[idx] = false
-                event.srcElement.style.backgroundImage = "url('')"
+            if (color == "black" || color == "white") {
+                number = getRandom(6, 0) + 1
             } else {
-                arrLiberalEnacted[idx] = true
-                event.srcElement.style.backgroundImage = "url('img/secrethitler/liberalpolicys.png')"
-                let btnElectTrack = document.getElementById("btnElect" + curElectionTracker)
-                btnElectTrack.style.backgroundColor = "transparent"
-                btnElectTrack.style.boxShadow = " 0px 0px transparent"
-
-                curElectionTracker = 0
-                btnElectTrack = document.getElementById("btnElect" + curElectionTracker)
-                btnElectTrack.style.backgroundColor = "#2980B9"
-                btnElectTrack.style.boxShadow = shadowOffset + "px " + shadowOffset + "px gray"
+                number = getRandom(3, 0) + 1
             }
+            console.log(color, number)
         }
+
+        occupied["btnPropChip_" + i] = color + number
+        let chip = document.getElementById("btnPropChip_" + i)
+        chip.style.backgroundImage = "url('img/readysetbet/chips/" + color + " (" + number + ").png')"
+        chip.style.boxShadow = shadowDepth + "px " + shadowDepth + "px gray"
+
     }
-
-    locArr = new Array(0.0886, 0.6228, 0.22652, 0.8878)
-    length = locArr[2] - locArr[0];
-    for (let idx = 0; idx < 6; idx++) {
-        //let btnLiberal = 
-        funcInsertElement("btnFascist" + idx, "button", "btnTrans",
-            locArr[0] + idx * length,
-            locArr[1],
-            locArr[2] + idx * length,
-            locArr[3]
-        ).onclick = function() {
-            if (arrFascistEnacted[idx] == true) {
-                arrFascistEnacted[idx] = false
-                event.srcElement.style.backgroundImage = "url('')"
-            } else {
-                arrFascistEnacted[idx] = true
-                event.srcElement.style.backgroundImage = "url('img/secrethitler/fascistpolicys.png')"
-
-                let btnElectTrack = document.getElementById("btnElect" + curElectionTracker)
-                btnElectTrack.style.backgroundColor = "transparent"
-                btnElectTrack.style.boxShadow = " 0px 0px transparent"
-
-                curElectionTracker = 0
-                btnElectTrack = document.getElementById("btnElect" + curElectionTracker)
-                btnElectTrack.style.backgroundColor = "#2980B9"
-                btnElectTrack.style.boxShadow = shadowOffset + "px " + shadowOffset + "px gray"
-
-
-            }
-        }
-    }
-
-    locArr = new Array(0.3321, 0.3907, 0.3707, 0.4429)
-    length = 0.094
-
-    let shadowOffset = 0.004 * pageWidth
-    for (let idx = 0; idx < 4; idx++) {
-        //let btnLiberal = 
-        funcInsertElement("btnElect" + idx, "button", "btnCircle",
-            locArr[0] + idx * length,
-            locArr[1],
-            locArr[2] + idx * length,
-            locArr[3]
-        ).onclick = function() {
-            if (idx + 1 == curElectionTracker || idx - 1 == curElectionTracker) {
-                let btnElectTrack = document.getElementById("btnElect" + curElectionTracker)
-                btnElectTrack.style.backgroundColor = "transparent"
-                btnElectTrack.style.boxShadow = " 0px 0px transparent"
-
-                curElectionTracker = idx
-                btnElectTrack = document.getElementById("btnElect" + curElectionTracker)
-                btnElectTrack.style.backgroundColor = "#2980B9"
-                btnElectTrack.style.boxShadow = shadowOffset + "px " + shadowOffset + "px gray"
-            }
-        }
-    }
-    let btnElectTrack = document.getElementById("btnElect" + curElectionTracker)
-    btnElectTrack.style.backgroundColor = "#2980B9"
-    btnElectTrack.style.boxShadow = shadowOffset + "px " + shadowOffset + "px gray"
-    funcInsertFullScreenButton(0.02,0.47,0.08,0.525)
-    
-
 
 }
 
-fucDrawSecretHitlerBoard()
+function funcColorBet(i, color, number) {
+    console.log("funcColor", i)
+    if (occupied["btnColorBetChip_" + i] != undefined) {
+        return
+    }
+
+    if (color == undefined) {
+        color = colors[getRandom(colors.length, 0)]
+
+        if (color == "black" || color == "white") {
+            number = getRandom(6, 0) + 1
+        } else {
+            number = getRandom(3, 0) + 1
+        }
+        console.log(color, number)
+    }
+
+    occupied["btnColorBetChip_" + i] = color + number
+    let chip = document.getElementById("btnColorBetChip_" + i)
+    chip.style.backgroundImage = "url('img/readysetbet/chips/" + color + " (" + number + ").png')"
+    chip.style.boxShadow = shadowDepth + "px " + shadowDepth + "px gray"
+
+}
+
+function funcBet(x, y, color, number) {
+    console.log("funcBet", x,y)
+    if (occupied["btnBet_" + x + "_" + y] != undefined) {
+        return
+    }
+
+    if (color == undefined) {
+        color = colors[getRandom(colors.length, 0)]
+
+        if (color == "black" || color == "white") {
+            number = getRandom(6, 0) + 1
+        } else {
+            number = getRandom(3, 0) + 1
+        }
+        console.log(color, number)
+    }
+
+    occupied["btnBet_" + x + "_" + y] = color + number
+    let chip = document.getElementById("btnBet_" + x + "_" + y)
+    chip.style.backgroundImage = "url('img/readysetbet/chips/" + color + " (" + number + ").png')"
+    chip.style.boxShadow = shadowDepth + "px " + shadowDepth + "px gray"
+}
+
+let curRace = 0
+function funcExotic(i, color, number) {
+    console.log("funcExotic", i)
+    if (i==0){
+        if (curRace==4){
+            return
+        }
+        for (let key in occupied){
+            console.log(key)
+            let chip = document.getElementById(key)
+            chip.style.backgroundImage="url('')"
+            chip.style.boxShadow=""
+        }
+        occupied={}
+        ++curRace
+        let exotic = document.getElementById("btnExotic_" + curRace)
+        exotic.style.backgroundImage="url('img/readysetbet/exotic/ex"+exoticOrder[curRace-1]+".png')"
+
+        for (let j = 1; j <= 5; j++) {
+            let prop = document.getElementById("btnProp_" + j)
+            prop.style.backgroundImage="url('img/readysetbet/propbets/pro ("+propOrder[j-1+5*(curRace-1)]+").png')"
+            
+        }
+        return
+    }
+    if (i>=1 && i<=curRace){
+        for (let t = 0; t < 3; t++) {
+            if (occupied["btnExoticChip_" + i + "_"+t] == undefined) {
+                if (color == undefined) {
+                    color = colors[getRandom(colors.length, 0)]
+            
+                    if (color == "black" || color == "white") {
+                        number = getRandom(6, 0) + 1
+                    } else {
+                        number = getRandom(3, 0) + 1
+                    }
+                    console.log(color, number)
+                }
+                occupied["btnExoticChip_" + i + "_"+t]=color+number
+                let chip = document.getElementById("btnExoticChip_" + i + "_"+t)
+                chip.style.backgroundImage = "url('img/readysetbet/chips/" + color + " (" + number + ").png')"
+                chip.style.boxShadow = shadowDepth2 + "px " + shadowDepth2 + "px gray"
+                return
+
+            }
+        
+        }
+    
+    }
+}
+
+function fucDrawReadySetBetBoard() {
+
+
+    let board = funcInsertElement("imgBoard", "img", "null", 0, 0, 1, 1, pageWidth / pageHeight)
+    board.src = "img/readysetbet/main.jpg"
+
+    let xGap = 0.1658
+    for (let i = 0; i < 6; i++) {
+        let btnExotic = funcInsertElement("btnExotic_" + i, "button", "btnTrans", 0.015 + i * xGap, 0.86, 0.155 + i * xGap, 0.99)
+        btnExotic.onclick=function(){funcExotic(i)}
+        if (i == 0) {
+            btnExotic.style.backgroundImage = "url('img/readysetbet/exotic/bg.png')"
+        } else if (i == 5) {
+            btnExotic.style.backgroundImage = "url('img/readysetbet/vip/bg.png')"
+            btnExotic.onclick = function(){funcFullScreen()}
+        }
+
+        if (i>=1 && i<=4){
+        let xLoc = (0.015 + 0.155 - coinSizePerWidth) / 2
+
+        let btnExoticChip1 = funcInsertElement("btnExoticChip_" + i + "_0", "button", "btnCircle", xLoc + i * xGap, 0.905, xLoc + coinSizePerWidth + i * xGap, 0.99, 1)
+
+        xLoc = xLoc - 0.195 + xGap
+        let btnExoticChip2 = funcInsertElement("btnExoticChip_" + i + "_1", "button", "btnCircle", xLoc + i * xGap, 0.945, xLoc + coinSizePerWidth + i * xGap, 0.99, 1)
+
+        xLoc = xLoc + 0.20 + coinSizePerWidth / 2 - xGap
+        let btnExoticChip3 = funcInsertElement("btnExoticChip_" + i + "_2", "button", "btnCircle", xLoc + i * xGap, 0.945, xLoc + coinSizePerWidth + i * xGap, 0.99, 1)
+
+        btnExoticChip1.onclick=function(){funcExotic(i)}
+        btnExoticChip2.onclick=function(){funcExotic(i)}
+        btnExoticChip3.onclick=function(){funcExotic(i)}
+
+
+        }
+
+
+
+    }
+
+    xGap = 0.164
+    for (let i = 0; i < 6; i++) {
+        let btnProp = funcInsertElement("btnProp_" + i, "button", "btnTrans", 0.015 + i * xGap, 0.0084, 0.165 + i * xGap, 0.0703)
+        if (i == 0) {
+            btnProp.style.backgroundImage = "url('img/readysetbet/propbets/bg.png')"
+        }
+        btnProp.onclick = function () { funcProp(i) }
+        let xLoc = (0.015 + 0.165) / 2 - coinSizeOrigin / 2
+        let btnPropChip = funcInsertElement("btnPropChip_" + i, "button", "btnCircle", xLoc + i * xGap, 0.0084, xLoc + coinSizeOrigin + i * xGap, 0.0703, 1)
+        btnPropChip.onclick = function () { funcProp(i) }
+
+
+    }
+
+    xGap = 0.218
+    for (let i = 0; i < 4; i++) {
+        if (i >= 2) {
+            xGap = 0.21
+        }
+        let btnColorBet = funcInsertElement("btnColorBet_" + i, "button", "btnTrans", 0.1007 + i * xGap, 0.0829, 0.2707 + i * xGap, 0.1427)
+        btnColorBet.onclick = function () { funcColorBet(i) }
+        let xLoc = (0.1007 + 0.2707 - coinSizeOrigin) / 2
+        let btnColorBetChip = funcInsertElement("btnColorBetChip_" + i, "button", "btnCircle", xLoc + i * xGap, 0.0841, xLoc + i * xGap + coinSizeOrigin, 0.1427, 1)
+        btnColorBetChip.onclick = function () { funcColorBet(i) }
+    }
+
+    xGap = 0.095
+    let xGap2 = 0.02
+    let yGap = 0.065
+    let yGap2 = 0.008
+    for (let x = 0; x < 7; x++) {
+        for (let y = 0; y < 9; y++) {
+            let xLoc = 0.0929 + x * xGap
+            let xLoc2 = 0.1826 + x * xGap
+            xLoc2 = xLoc + coinSizeOrigin
+            if (x >= 2) {
+                xLoc += xGap2
+                xLoc2 += xGap2
+            }
+            if (x >= 4) {
+                xLoc += xGap2
+                xLoc2 += xGap2
+            }
+
+            let yLoc = 0.2004 + y * yGap
+            let yLoc2 = 0.2634 + y * yGap
+            if (y >= 2) {
+                yLoc += yGap2
+                yLoc2 += yGap2
+            }
+            if (y >= 7) {
+                yLoc += yGap2
+                yLoc2 += yGap2
+            }
+            let btnBet = funcInsertElement("btnBet_" + x + "_" + y, "button", "btnCircle", xLoc, yLoc, xLoc2, yLoc2, 1)
+            btnBet.onclick = function () { funcBet(x, y) }
+        }
+    }
+
+}
+
+fucDrawReadySetBetBoard()
---
+++ readysetbetInd.js
@@ -0,0 +1,236 @@
+
+var webSocket = new WebSocket("ws://192.168.1.139:9998");
+
+webSocket.onopen = function(message){      
+    console.log("Server connect...")    
+};
+
+webSocket.onmessage = function(message){      // 출력 area에 메시지를 표시한다.      
+    console.log("Recieve From Server => "+message.data)    
+};
+
+pageHeight = document.documentElement.clientHeight
+pageWidth = document.documentElement.clientWidth
+
+mainBody.style.border = "0px"
+mainBody.style.margin = "0px"
+mainBody.style.paddingTop ="0px"
+
+mainDiv.style.border = "0px"
+mainDiv.style.margin = "0px"
+mainDiv.style.paddingTop = pageWidth/5+"px" 
+
+let ratios=[]
+ratios[0]=[[[9,2],[8,2],[7,2]],[[5,3],[5,4]],[[4,3],[4,4]]]
+ratios[1]=[[[7,0],[6,0],[5,1]],[[4,0],[4,1]],[[3,0],[3,1]]]
+ratios[2]=[[[5,0],[4,0],[4,2]],[[3,2],[2,2]],[[2,0],[2,3]]]
+ratios[3]=[[[3,0],[3,1],[3,2]],[[2,1],[2,5]],[[1,0],[1,2]]]
+ratios[4]=[[[3,2],[3,3],[3,4]],[[2,5],[2,6]],[[1,1],[1,3]]]
+ratios[5]=ratios[3]
+ratios[6]=ratios[2]
+ratios[7]=ratios[1]
+ratios[8]=ratios[0]
+
+let selectCoin = 0
+let idcolor = "green"
+let currentAvailabelSlots=[]
+for (let i = 0; i < 9; i++) {
+    currentAvailabelSlots.push([0,0,0])
+}
+
+function funcBet(color,selectCoidID,i,j){
+    console.log(color,selectCoidID,i,j)
+    webSocket.send(color+"_"+selectCoidID+"_"+i+"_"+j);
+}
+
+
+
+function funcSlotOccupied(i,j){
+    let td = document.getElementById("betslot_"+i+"_"+j)
+    
+    if (j==0 && currentAvailabelSlots[i][j]==2){
+        td.innerHTML="-"
+        return
+    } else if (j>0 && currentAvailabelSlots[i][j]==1){
+        td.innerHTML="-"
+        return
+    } 
+
+    currentAvailabelSlots[i][j]++
+    
+
+    td.innerHTML=ratios[i][j][currentAvailabelSlots[i][j]][0]+"x"
+    if (ratios[i][j][currentAvailabelSlots[i][j]][1]>0){
+        let btn = funcAddChild("button",td)
+        btn.className="btnCircle"
+        btn.innerHTML="-"+ratios[i][j][currentAvailabelSlots[i][j]][1]
+        btn.style.backgroundColor="red"
+        btn.style.width = btn.style.height = pageWidth/20+"px"
+        btn.style.color = "white"
+    }
+
+}
+
+function funcAddChild(type,parent){
+    let child = document.createElement(type)
+    parent.appendChild(child)
+    return child
+}
+function funcDraw(){
+    let headerChip = document.getElementById("headChips")
+    
+    headerChip.style.margin="0px"
+    headerChip.style.padding="0px"
+    headerChip.style.border="0px"
+    headerChip.style.height = pageWidth/5+"px"
+    let tblChip = funcAddChild("table",headerChip)
+    tblChip.style.width = "100%"
+    let trChip = funcAddChild("tr",tblChip)
+    for (let i = 0; i < 5; i++) {
+        let tdChip = funcAddChild("td",trChip)
+        tdChip.style.width="10%"
+        
+        let img = funcAddChild("img",tdChip)
+        img.src = "img/readysetbet/chips/green ("+(i+1)+").png"
+        if (i==4){
+            img.src = "img/readysetbet/chips/black ("+(i)+").png"
+        }
+        img.style.width = pageWidth/6+"px"
+        img.style.borderRadius="100%"
+        img.style.boxShadow="10px 10px gray"
+
+    }
+
+    let tblMain = document.getElementById('tblInd')
+    tblMain.style.width = "100%"
+    let tr = funcAddChild("tr",tblMain)
+    let td = funcAddChild("td",tr)
+    let tblProp = funcAddChild("table",td)
+    tr = funcAddChild("tr",tblProp)
+    for (let i = 0; i < 3; i++) {
+        td = funcAddChild("td",tr)
+        td.style.width="10%"
+        let img = funcAddChild("img",td)
+        img.src = "img/readysetbet/propbets/pro ("+i+").png"
+        img.style.width = pageWidth/3.2+"px"
+    }
+    tr = funcAddChild("tr",tblMain)
+    td = funcAddChild("td",tr)
+    
+    let tblProp2 = funcAddChild("table",td)
+    tr = funcAddChild("tr",tblProp2)
+    for (let i = 0; i < 2; i++) {
+        td = funcAddChild("td",tr)
+        td.style.width="10%"
+        td.style.textAlign="right"
+        if (i==1){
+            td.style.textAlign="left"
+        }
+        let img = funcAddChild("img",td)
+        img.src = "img/readysetbet/propbets/pro ("+(i+5)+").png"
+        img.style.width = pageWidth/3.2+"px"
+    }
+
+    tr = funcAddChild("tr",tblMain)
+    td = funcAddChild("td",tr)
+    let tblInd = funcAddChild("table",td)
+    tr = funcAddChild("tr",tblInd)
+    td = funcAddChild("td",tr)
+    td.style.width="10%"
+    let img = funcAddChild("img",td)
+    img.src = "img/readysetbet/horse/blue.png"
+    img.style.width = pageWidth/4.4+"px"
+    td = funcAddChild("td",tr)
+    td.style.width="10%"
+    img = funcAddChild("img",td)
+    img.src = "img/readysetbet/horse/orange.png"
+    img.style.width = pageWidth/4.4+"px"
+    td = funcAddChild("td",tr)
+    td.style.width="10%"
+    img = funcAddChild("img",td)
+    img.src = "img/readysetbet/horse/red.png"
+    img.style.width = pageWidth/4.4+"px"
+    td = funcAddChild("td",tr)
+    td.style.width="10%"
+    img = funcAddChild("img",td)
+    img.src = "img/readysetbet/horse/black.png"
+    img.style.width = pageWidth/4.4+"px"
+
+    tr = funcAddChild("tr",tblInd)
+
+    td = funcAddChild("td",tr)
+    td.innerHTML="SHOW"
+    td.style.fontSize = pageWidth/15+"px"
+    td.className = "fontSHOW"
+
+    td = funcAddChild("td",tr)
+    td.innerHTML="PLACE"
+    td.style.fontSize = pageWidth/15+"px"
+    td.className = "fontPLACE"
+    td = funcAddChild("td",tr)
+    td.innerHTML="WIN"
+    td.style.fontSize = pageWidth/15+"px"
+    td.className = "fontWIN"
+
+
+    for (let i = 0; i < 9; i++) {
+
+        tr = funcAddChild("tr",tblInd)
+
+        td = funcAddChild("td",tr)
+        td.innerHTML=ratios[i][2][0][0]+"x"
+        td.id = "betslot_"+i+"_2"
+        td.style.fontSize = pageWidth/8+"px"
+        td.className = "fontSHOW"
+        if (ratios[i][2][0][1]>0){
+            let btn = funcAddChild("button",td)
+            btn.className="btnCircle"
+            btn.innerHTML="-"+ratios[i][2][0][1]
+            btn.style.backgroundColor="red"
+            btn.style.width = btn.style.height = pageWidth/20+"px"
+            btn.style.color = "white"
+        }
+    
+        td = funcAddChild("td",tr)
+        td.innerHTML=ratios[i][1][0][0]+"x"
+        td.id = "betslot_"+i+"_1"
+        td.style.fontSize = pageWidth/8+"px"
+        td.className = "fontPLACE"
+        if (ratios[i][1][0][1]>0){
+            let btn = funcAddChild("button",td)
+            btn.className="btnCircle"
+            btn.innerHTML="-"+ratios[i][1][0][1]
+            btn.style.backgroundColor="red"
+            btn.style.width = btn.style.height = pageWidth/20+"px"
+            btn.style.color = "white"
+        }
+    
+        td = funcAddChild("td",tr)
+        td.innerHTML=ratios[i][0][0][0]+"x"
+        td.id = "betslot_"+i+"_0"
+        if (ratios[i][0][0][1]>0){
+            let btn = funcAddChild("button",td)
+            btn.className="btnCircle"
+            btn.innerHTML="-"+ratios[i][0][0][1]
+            btn.style.backgroundColor="red"
+            btn.style.width = btn.style.height = pageWidth/20+"px"
+            btn.style.color = "white"
+        }
+        td.style.fontSize = pageWidth/8+"px"
+        td.className = "fontWIN"
+        td.onclick=function(){funcBet(idcolor,selectCoin,i,0)}
+ 
+
+        td = funcAddChild("td",tr)
+        td.style.width="10%"
+        let img = funcAddChild("img",td)
+        img.src = "img/readysetbet/horse/horse"+i+".png"
+        img.style.width = pageWidth/4.2+"px"
+    
+    }
+
+
+
+}
+
+funcDraw()
